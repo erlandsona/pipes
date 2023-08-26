67dbb48f0dacdc363de6a77bc971b6c97eb9b133
Austin Erlandson <austinerlandson77@gmail.com>
2023-08-26 16:21:22 -0500

    It's the beginning of a very beautiful friendship.

--- test/Main.hs
+++ test/Main.hs
@@ -20,7 +20,9 @@ main = do
     Test.Tasty.defaultMain test
 
 spec :: Spec
-spec = parallel $ do
-    it "Test Parsing MyProgram.|" $ do
+spec = parallel do
+    it "Test Parsing MyProgram.|" do
         result <- go "test/MyProgram.|"
         result `shouldBe` Right (Or (Add (Nat 123) (Eq (Mul (Nat 1) (Nat 5)) (Boo True))) (Eq (Nat 4) (Nat 5)))
+    it "reduces" do
+        True `shouldBe` False
