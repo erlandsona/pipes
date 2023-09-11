0e53b6f12d67d08cfbd41a0a4fd09df4d22c81d3
Austin Erlandson <austinerlandson77@gmail.com>
2023-09-10 19:53:30 -0500

    fourmolu and lsps still borked :/

---
+++ test/Main.hs
@@ -0,0 +1,103 @@
+import Relude
+
+-- Tasty makes it easy to test your code. It is a test framework that can
+-- combine many different types of tests into one suite. See its website for
+-- help: <http://documentup.com/feuerbach/tasty>.
+import Test.Tasty qualified
+
+-- Hspec is one of the providers for Tasty. It provides a nice syntax for
+-- writing tests. Its website has more info: <https://hspec.github.io>.
+
+import Test.Hspec
+
+-- import Test.Hspec.Megaparsec
+import Test.Tasty.Hspec
+
+import Data.String.Interpolate
+import Text.Gigaparsec
+
+import Ast
+import Lam qualified
+
+main :: IO ()
+main = do
+    test <- testSpec "pipes" spec
+    Test.Tasty.defaultMain test
+
+spec :: Spec
+spec = parallel do
+    it "Test Parsing Identity" do
+        let
+            in1 = "x =  x"
+            in2 = "(x= x)"
+            in3 =
+                [__i|
+                  -- Identity
+                  (x= x)
+                |]
+            output = lams [name "x"] (ref "x")
+        parse Lam.expr in1 `shouldParse` (output)
+        parse Lam.expr in2 `shouldParse` (output)
+        parse Lam.expr in3 `shouldParse` (output)
+
+    it "Test Parsing Const" do
+        let
+            in1 = "x y =  x"
+            in2 = "(x = (y = x))"
+            in3 =
+                [__i|
+                  -- Const
+                  (x= (y = x))
+                |]
+            output = lams [name "x", name "y"] (ref "x")
+        parse Lam.expr in1 `shouldParse` (output)
+        parse Lam.expr in2 `shouldParse` (output)
+        parse Lam.expr in3 `shouldParse` (output)
+
+    it "Test Parsing Application" do
+        let
+            in1 = "(x y = x) z = z"
+            in2 = "(x= (y = x)) (z= z)"
+            in3 =
+                [__i|
+                  -- Application
+                  x y = x
+                    z = z
+                |]
+            output = app (lams [name "x", name "y"] (ref "x")) (lams [name "z"] (ref "z"))
+        parse Lam.expr in1 `shouldParse` (output)
+        parse Lam.expr in2 `shouldParse` (output)
+        parse Lam.expr in3 `shouldParse` (output)
+
+    it "Test parsing flip" do
+        let
+            in1 = "fn x y = fn y x"
+            output =
+                lams
+                    [name "fn", name "x", name "y"]
+                    (app (ref "fn") (app (ref "y") (ref "x")))
+        parse Lam.expr in1 `shouldParse` (output)
+
+-- TODO: Work out flip above.
+-- it "Test binding infix operators" do
+--   let
+--     in1 =
+--       [__i|
+--         (\\ x y = x \\ y)
+--           -- flip ($) aka: (&)
+--           ((fn a b = fn b a) (fn a = fn a))
+--       |]
+--     out = app (lams [name "\\", name "x", name "y"] (app (ref "x"))) (ref "y")
+--   parse Lam.expr in1 `shouldParse` out
+
+shouldParse
+    :: (Show a, Eq a)
+    => Result a
+    -- ^ Result of parsing as returned by function like 'parse'
+    -> a
+    -- ^ Desired result
+    -> Expectation
+r `shouldParse` v = case r of
+    Failure ->
+        expectationFailure $ "expected: " ++ show v
+    Success x -> x `shouldBe` v
